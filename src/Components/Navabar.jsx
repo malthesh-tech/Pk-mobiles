40196e02b1fbfec9143831d7772266afce57909c
malthesh-tech <maltheshcb350@gmail.com>
2025-03-09 18:54:03 +0530

    Updated navbar for mobile screen

--- src/Components/Navabar.jsx
+++ src/Components/Navabar.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import logo from '../assets/PK=Mobileds.png'
 import { FaChevronDown } from "react-icons/fa";
 import { FaSearch, FaShoppingCart, FaRegBookmark, FaUserCircle } from "react-icons/fa";
+import './Navbar.css'
 
 const Navabar = () => {
   return (
 
-    <div className='w-full bg-black flex flex-wrap items-center px-4 py-3 md:px-10 justify-between' >  
+    <div className='masterDiv w-full bg-black flex flex-wrap items-center px-20 py-2 justify-between' id='masterDiv' > {/* flex flex-wrap items-center px-4 py-3 md:px-10 justify-between */}
      {/* w-full bg-black flex flex-wrap items-center justify-between px-4 py-3 md:px-10  */}
-     <div className="flex flex-col md:flex-row items-center md:items-start gap-2">
-       <img src={logo} alt="logo"  className='w-12,  h-20 '/> 
+     <div className="imgeDiv flex flex-row items-start gap-2" >
+       <img src={logo} alt="logo"  className='imgTT w-12,  h-20 '/> 
       
-       <div className='flex flex-col md:flex-row md:ml-0 ml-6 md:pl-20 mt-2 md:mt-5'>
-        <h1 className='text-white text-4xl  md:text-2xl font-bold'>30 Minutes</h1>
+       <div className='Minutes flex flex-row ml-0 pl-20 mt-5'>
+        <h1 className='text-white text-xl font-bold'>30 Minutes</h1>
         <FaChevronDown size={16} color="white" className="ml-2 mt-2"/>
        </div>
        </div>
@@ -23,19 +24,19 @@ const Navabar = () => {
           <FaChevronDown size={16} color="white" className="md:ml-2" />
         </div>
       </div> */}
-       <div className="flex items-center bg-white rounded-lg p-2 w-full max-w-xs md:max-w-md mt-4 md:mt-5 h-12 ">
+       <div className=" SearchIcon  flex items-center bg-white rounded-lg p-2 w-full max-w-2xl mt-5 h-12  ">
           <FaSearch className="text-grey mr-2 " size={20} />
           <input
             type="text"
             placeholder="Search here..."
-            className="flex-grow outline-none px-2 text-sm md:text-base"
+            className="flex-grow outline-none px-2 text-base"
           />
           </div>
-       <div className='flex mt-6 gap-3'>
-       <FaRegBookmark color='white' size={24}/>
+       <div className='Profile-Cart flex mt-6 gap-3'>
+       <FaRegBookmark color='white' size={24} className='Bookmark'/>
        <FaShoppingCart color='white' size={24} />
        <FaUserCircle color='white' size={24} />
-       <FaChevronDown size={16} color="white" className="ml-2"/>
+       <FaChevronDown size={16} color="white" className="Bookmark ml-2"/>
        </div>
     </div> 
       
